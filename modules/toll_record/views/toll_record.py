fbdef260037f0f3d7c3514ff19b7b1f0a2e5f9ea
Anthony Terán <108620290+aterana3@users.noreply.github.com>
2024-11-13 18:41:24 -0500

    Quitada la paginación de la vista de TollRecord

--- modules/toll_record/views/toll_record.py
+++ modules/toll_record/views/toll_record.py
@@ -6,14 +6,11 @@ class TollRecordListView(ListView):
     model = TollRecord
     template_name = 'toll_records/page-list.html'
     context_object_name = 'toll_records'
-    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
         license_plate = self.request.GET.get('license_plate')
         if license_plate:
             queryset = queryset.filter(license_plate__plate_number__icontains=license_plate)
-        else:
-            queryset = queryset.none()
         return queryset.order_by('pass_date')
 
